2c4611adaad8284ddb51b45f39767e97be398b75
Nikilesh Ramesh <nikileshramesh2003@gmail.com>
2024-07-29 18:39:05 +0300

    poster

--- math/CostVisualizations.m
+++ math/CostVisualizations.m
@@ -13,6 +13,39 @@ k2 = [1, 10, 100, 1000];
 rho = [1,5,10]
 xr = 1.5;
 
+%% New comp
+figure(15)
+xlim([-5,5])
+
+p5= patch([-dt.*ones(size(f)), fliplr(min(xlim).*ones(size(f)))]+xr, [linspace(-5, f(1).^2, length(f)), fliplr(linspace(-1, f(1).^2, length(f)))], [1, 0, 0] , 'FaceAlpha', 0.15, 'EdgeColor', 'none');
+hold on
+p6 = patch([+dt.*ones(size(f)), fliplr(max(xlim).*ones(size(f)))]+xr, [linspace(-5, f(1).^2, length(f)), fliplr(linspace(-1, f(1).^2, length(f)))], [1, 0, 0] , 'FaceAlpha', 0.15, 'EdgeColor', 'none');
+
+cost = f.^2; %set point
+p1 = plot(f+xr,cost, LineWidth=2, Color=[0 0.4470 0.7410]);
+cost = J(f2, 1e09); % Lr
+p2 = plot(f2+xr,cost, LineWidth=2,Color=[0.4660 0.6740 0.1880]);
+out = soft_max((f.^2 - dt.^2).*alpha, 0,50); % soft const
+p3 = plot(f+xr,out, LineWidth=2, Color=[ 0.8500 0.3250 0.0980]); 
+p4 = xline(1.5, 'r--',"Reference",LineWidth=2); %ref
+
+
+idx1 = find(f>=0,1);
+idx2 = find(f<=3,1,"last");
+hard = zeros(size(f(idx1:idx2)));
+
+p7 = plot(f(idx1:idx2), hard, LineWidth=2, Color=[0.5,0,0.5]);
+p8 = plot([0;0], [0; max(ylim)], LineWidth=2, Color=[0.5,0,0.5]);
+p9 = plot([3;3], [0; max(ylim)], LineWidth=2, Color=[0.5,0,0.5]);
+hold off
+grid on
+xlabel("Position [m]",FontSize=12,FontWeight="bold",Interpreter="latex")
+ylabel('Lagrange Cost',FontSize=12,FontWeight="bold",Interpreter="latex")
+xlim([-2 5])
+ylim([-2.5 10])
+legend([p1, p2, p3,p8, p4, p5] ,["Setpoint tracking","In-range tracking (n.a.i.r)", "In-range tracking (a.i.r) with soft constraints", "In-range tracking (a.i.r) with hard constraints", "Reference", "Out of range"], 'Location','northoutside', 'NumColumns', 2,  'Interpreter','latex','FontSize',11)
+
+
 %% Tracking cost comparison
 figure(1)
 xlim([-5,5])
